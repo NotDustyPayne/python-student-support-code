6b47791214509b82aa72784c67a78819a4618197
Dusty Payne <coachpayne510@gmail.com>
2023-07-03 19:07:00 -0700

    1.1 impl

--- interp_Lint.py
+++ interp_Lint.py
@@ -1,6 +1,56 @@
 from ast import *
 from utils import input_int, add64, sub64, neg64
 
+def pe_neg(r):
+  match r:
+    case Constant(n):
+      return Constant(neg64(n))
+    case _:
+      return UnaryOp(USub(), r)
+     
+
+def pe_add(r1, r2):
+  match (r1, r2):
+    case (Constant(n1), Constant(n2)):
+        return Constant(add64(n1, n2))
+    case _:
+      return BinOp(r1, Add(), r2)
+     
+def pe_sub(r1, r2):
+   match (r1, r2):
+    case (Constant(n1), Constant(n2)):
+      return Constant(sub64(n1, n2))
+    case _:
+      return BinOp(r1, Sub(), r2)
+
+def pe_exp(e):
+  match e:
+    case BinOp(left, Add(), right):
+      return pe_add(pe_exp(left), pe_exp(right))
+    case BinOp(left, Sub(), right):
+      return pe_sub(pe_exp(left), pe_exp(right))
+    case UnaryOp(USub(), v):
+      return pe_neg(pe_exp(v))
+    case Constant(value):
+      return e
+    case Call(Name('input_int'), []):
+      return e
+     
+def pe_stmt(s):
+  match s:
+    case Expr(Call(Name('print'), [arg])):
+      return Expr(Call(Name('print'), [pe_exp(arg)]))
+    case Expr(value):
+      return Expr(pe_exp(value))
+
+
+def pe_P_int(p):
+  match p:
+    case Module(body):
+      new_body = [pe_stmt(s) for s in body]
+      return Module(new_body)
+         
+
 def interp_exp(e):
     match e:
         case BinOp(left, Add(), right):
@@ -29,7 +79,7 @@ def interp_stmt(s):
 
 def interp(p):
     match p:
-        case Module(body):
+        case Module(body):           
             for s in body:
                 interp_stmt(s)
         case _:
@@ -92,3 +142,23 @@ if __name__ == "__main__":
   pr = Expr(Call(Name('print'), [ast1_1]))
   p = Module([pr])
   interp(p)
+
+
+# 1.1, verify partial eval is the same as regular eval with 3 programs
+# because statements in Lint are evaluated but the results are ignored, Im just manually checking
+# this. (interp results do not get returned)
+
+p1 = BinOp(BinOp(Constant(7), Add(), Constant(1)), Add(), BinOp( Constant(10),Sub(), Constant(4)))
+p2 = UnaryOp(USub(), BinOp(Constant(32), Sub(), Constant(14)))
+p3 = BinOp(BinOp(Constant(7), Add(), Constant(1)), Add(), UnaryOp(USub(), Constant(3)))
+
+programs = [p1,p2,p3]
+
+def partials_test():
+   for program in programs:
+      normal_p = Module([Expr(Call(Name('print'), [program]))])
+      partial_p = pe_P_int(Module([Expr(Call(Name('print'), [program]))]))
+      print(f"result of program {program} :")
+      interp(normal_p)
+      interp(partial_p)
+
