c6756a3d86778742c015517b40b8f8cf22e36a07
Dusty Payne <coachpayne510@gmail.com>
2023-10-15 16:57:23 -0700

    rco implemented

--- compiler.py
+++ compiler.py
@@ -15,17 +15,81 @@ class Compiler:
     # Remove Complex Operands
     ############################################################################
 
+
+    def temps_helper(self, temp_name, temp_expr):
+        """
+        makes each temp binding an assignment statement
+        """
+        return Assign([temp_name], temp_expr)
+
     def rco_exp(self, e: expr, need_atomic: bool) -> Tuple[expr, Temporaries]:
         # YOUR CODE HERE
-        raise Exception('rco_exp not implemented')
+        """
+        use utils.generate_name for fresh stub names
+        """
+        if need_atomic is False:
+            return (e,[])
+        match e:
+            # atomics
+            case Constant(n):
+                return (Constant(n), [])
+            case Name(var):
+                return (Name(var), [])
+            # expressions
+            case Call(Name('input_int'), []):
+                input_var = Name(generate_name('input_int_var'))
+                return input_var, [(input_var, Call(Name('input_int'), []))]
+            case UnaryOp(USub(), exp):
+                # -10 -> tmp1, [(tmp1, -10)]
+                atomic, temps = self.rco_exp(exp, True)
+                unary_stub = Name(generate_name('unary_op'))
+                return unary_stub, temps + [(unary_stub, atomic)]
+            
+            case BinOp(left_exp,Add(), right_exp):
+                left_atomic, left_temps = self.rco_exp(left_exp, True)
+                right_atomic, right_temps = self.rco_exp(right_exp, True)
+
+                add_stub_var = Name(generate_name('binop_plus'))                
+                new_exp = BinOp(left_atomic, Add(), right_atomic)
+
+                return add_stub_var, left_temps + right_temps +[(add_stub_var, new_exp)]     
+
+            case BinOp(left_exp,Sub(), right_exp):
+                left_atomic, left_temps = self.rco_exp(left_exp, True)
+                right_atomic, right_temps = self.rco_exp(right_exp, True)
+
+                sub_stub_var = Name(generate_name('binop_sub'))                
+                new_exp = BinOp(left_atomic, Sub(), right_atomic)
+
+                return sub_stub_var, left_temps + right_temps + [(sub_stub_var, new_exp)] 
+
+        raise Exception('rco_exp error')
 
     def rco_stmt(self, s: stmt) -> List[stmt]:
-        # YOUR CODE HERE
-        raise Exception('rco_stmt not implemented')
 
-    # def remove_complex_operands(self, p: Module) -> Module:
-    #     # YOUR CODE HERE
-    #     raise Exception('remove_complex_operands not implemented')
+        match s:
+            case Expr(Assign(Name(var), exp)):
+                expression, temps = self.rco_exp(exp, False)
+                return [self.temps_helper(name, xpr) for name,xpr in temps] + [expr(Assign(Name(var), expression))]
+            case Expr(Call(Name('print'), [exp])):
+                atomic_exp, temps = self.rco_exp(exp, True)
+                return [self.temps_helper(name,xpr) for name,xpr in temps] + [Expr(Call(Name('print'), [atomic_exp]))]
+            case Expr(exp):
+                atomic_exp, temps = self.rco_exp(exp, True)
+                return [self.temps_helper(name,xpr) for name,xpr in temps] + [atomic_exp]
+
+                # return [self.rco_exp(exp, True)]
+        raise Exception('rco_stmt error')
+
+    def remove_complex_operands(self, p: Module) -> Module:
+        match p:
+            case Module(body):
+                rco_stmts = []
+                for stmt in body:
+                    rco_stmts += self.rco_stmt(stmt)
+                return Module(rco_stmts)
+            case _:
+                raise Exception('remove_complex_operands error')
         
 
     ############################################################################
